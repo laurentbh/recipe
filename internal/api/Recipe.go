6a66e6b8ba39a0c9a7fcb6495bfba03b306d7077
laurent buhler <lbuhler@gmail.com>
2021-05-27 17:35:25 -0700

    Fix time fields in recipe payload

--- internal/api/Recipe.go
+++ internal/api/Recipe.go
@@ -11,9 +11,9 @@ type Recipe struct {
 	Title       string      `json:"title,omitempty"`
 	Ingredients []string    `json:"ingredients,omitempty"`
 	Instruction string      `json:"instruction,omitempty"`
-	TotalTime   int         `json:"total_time,omitempty"`
-	PrepTime    int         `json:"prep_time,omitempty"`
-	CookTime    int		    `json:"cook_time,omitempty"`
+	TotalTime   string         `json:"total_time,omitempty"`
+	PrepTime    string         `json:"prep_time,omitempty"`
+	CookTime    string		    `json:"cook_time,omitempty"`
 	Yield       string      `json:"yield,omitempty"`
 	Tips        []string    `json:"tips,omitempty"`
 	References  []Reference `json:"references,omitempty"`
