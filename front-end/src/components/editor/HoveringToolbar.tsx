f362881adca5cba7ea08f6c80d55b2fd42f92043
laurent buhler <lbuhler@gmail.com>
2021-06-28 17:40:35 -0700

    Add hovering toolbar

---
+++ front-end/src/components/editor/HoveringToolbar.tsx
@@ -0,0 +1,148 @@
+import React, {useEffect, useRef, Ref, PropsWithChildren } from "react";
+import ReactDOM from "react-dom";
+import {ReactEditor, useSlate} from "slate-react";
+/** @jsx jsx */
+import { jsx, css, Global, ClassNames } from '@emotion/react'
+
+import {
+    Editor,
+    Transforms,
+    Text,
+    createEditor,
+    Element,
+    Descendant,
+} from 'slate'
+import { Range } from 'slate'
+import {Button, Icon} from "./components";
+
+const HoveringToolbar = () => {
+    const ref = useRef<HTMLDivElement | null>()
+    const editor = useSlate()
+
+    useEffect(() => {
+        const el = ref.current
+        const { selection } = editor
+
+        if (!el) {
+            return
+        }
+
+        if (
+            !selection ||
+            !ReactEditor.isFocused(editor) ||
+            Range.isCollapsed(selection) ||
+            Editor.string(editor, selection) === ''
+        ) {
+            el.removeAttribute('style')
+            return
+        }
+
+        const domSelection = window.getSelection()
+        // @ts-ignore
+        const domRange = domSelection.getRangeAt(0)
+        const rect = domRange.getBoundingClientRect()
+        el.style.opacity = '1'
+        el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`
+        el.style.left = `${rect.left +
+        window.pageXOffset -
+        el.offsetWidth / 2 +
+        rect.width / 2}px`
+    })
+
+    return (
+        <Portal>
+            <Menu
+                {...ref}
+                className={css`
+          padding: 8px 7px 6px;
+          position: absolute;
+          z-index: 1;
+          top: -10000px;
+          left: -10000px;
+          margin-top: -6px;
+          opacity: 0;
+          background-color: #222;
+          border-radius: 4px;
+          transition: opacity 0.75s;
+        `}
+            >
+                <FormatButton format="bold" icon="format_bold" />
+                <FormatButton format="italic" icon="format_italic" />
+                <FormatButton format="underlined" icon="format_underlined" />
+            </Menu>
+        </Portal>
+    )
+}
+export const Portal = ( children:any ) => {
+    return typeof document === 'object'
+        ? ReactDOM.createPortal(children, document.body)
+        : null
+}
+interface BaseProps {
+    className: string
+    [key: string]: unknown
+}
+type OrNull<T> = T | null
+export const Menu = React.forwardRef(
+    (
+        { className, ...props }: PropsWithChildren<BaseProps>,
+        ref: Ref<OrNull<HTMLDivElement>>
+    ) => (
+        <ClassNames>
+            {({ css, cx }) => (
+        <div
+            {...props}
+            {...ref}
+            // ref={ref}
+            className={cx(
+                className,
+                css`
+          & > * {
+            display: inline-block;
+          }
+
+          & > * + * {
+            margin-left: 15px;
+          }
+        `
+            )}
+        />
+            )}
+        </ClassNames>
+    )
+)
+
+const toggleFormat = (editor : any, format : string) => {
+    const isActive = isFormatActive(editor, format)
+    Transforms.setNodes(
+        editor,
+        { [format]: isActive ? null : true },
+        { match: Text.isText, split: true }
+    )
+}
+
+const isFormatActive = (editor : any, format : any) => {
+    // @ts-ignore
+    const [match] = Editor.nodes(editor, {
+        // @ts-ignore
+        match: n => n[format] === true,
+        mode: 'all',
+    })
+    return !!match
+}
+const FormatButton = ( format : any, icon : any ) => {
+    const editor = useSlate()
+    return (
+        <Button
+            reversed
+            active={isFormatActive(editor, format)}
+            // @ts-ignore
+            onMouseDown={event => {
+                event.preventDefault()
+                toggleFormat(editor, format)
+            }}
+        >
+            <Icon>{icon}</Icon>
+        </Button>
+    )
+}
---
+++ front-end/src/components/editor/components.tsx
@@ -0,0 +1,228 @@
+import React, { Ref, PropsWithChildren } from 'react'
+import ReactDOM from 'react-dom'
+/** @jsxImportSource @emotion/react */
+import { jsx, css, Global, ClassNames } from '@emotion/react'
+// import {  css } from '@emotion/react'
+// import { cx, css } from '@emotion/css/macro'
+
+interface BaseProps {
+  className: string
+  [key: string]: unknown
+}
+type OrNull<T> = T | null
+
+// @ts-ignore
+export const Button = React.forwardRef(
+  (
+    {
+      className,
+      active,
+      reversed,
+      ...props
+    }: PropsWithChildren<
+      {
+        active: boolean
+        reversed: boolean
+      } & BaseProps
+    >,
+    ref: Ref<OrNull<HTMLSpanElement>>
+  ) => (
+      <ClassNames>
+          {({ css, cx }) => (
+    <span
+      {...props}
+      {...ref}
+      className={cx(
+        className,
+        css`
+          cursor: pointer;
+          color: ${reversed
+            ? active
+              ? 'white'
+              : '#aaa'
+            : active
+            ? 'black'
+            : '#ccc'};
+        `
+      )}
+    />
+          )}
+      </ClassNames>
+  )
+)
+
+export const EditorValue = React.forwardRef(
+  (
+    {
+      className,
+      value,
+      ...props
+    }: PropsWithChildren<
+      {
+        value: any
+      } & BaseProps
+    >,
+    ref: Ref<OrNull<null>>
+  ) => {
+    const textLines = value.document.nodes
+        // @ts-ignore
+      .map(node => node.text)
+      .toArray()
+      .join('\n')
+      return (
+        <ClassNames>
+            {({ css, cx }) => (
+      <div
+        {...ref}
+        {...props}
+        className={cx(
+          className,
+          css`
+            margin: 30px -20px 0;
+          `
+        )}
+      >
+        <div
+            // @ts-ignore
+          className css={css`
+            font-size: 14px;
+            padding: 5px 20px;
+            color: #404040;
+            border-top: 2px solid #eeeeee;
+            background: #f8f8f8;
+          `}
+        >
+          Slate's value as text
+        </div>
+        <div
+            // @ts-ignore
+          className css={css`
+            color: #404040;
+            font: 12px monospace;
+            white-space: pre-wrap;
+            padding: 10px 20px;
+            div {
+              margin: 0 0 0.5em;
+            }
+          `}
+        >
+          {textLines}
+        </div>
+      </div>
+            )}
+        </ClassNames>
+    )
+  }
+)
+
+export const Icon = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<OrNull<HTMLSpanElement>>
+  ) => (
+      <ClassNames>
+          {({ css, cx }) => (
+    <span
+      {...props}
+      {...ref}
+      className={cx(
+        'material-icons',
+        className,
+        css`
+          font-size: 18px;
+          vertical-align: text-bottom;
+        `
+      )}
+    />
+          )}
+      </ClassNames>
+  )
+)
+
+export const Instruction = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<OrNull<HTMLDivElement>>
+  ) => (
+      <ClassNames>
+          {({ css, cx }) => (
+    <div
+      {...props}
+      {...ref}
+      className={cx(
+        className,
+        css`
+          white-space: pre-wrap;
+          margin: 0 -20px 10px;
+          padding: 10px 20px;
+          font-size: 14px;
+          background: #f8f8e8;
+        `
+      )}
+    />
+          )}
+      </ClassNames>
+  )
+)
+
+export const Menu = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<OrNull<HTMLDivElement>>
+  ) => (
+      <ClassNames>
+          {({ css, cx }) => (
+
+              <div
+      {...props}
+      {...ref}
+      className={cx(
+        className,
+        css`
+          & > * {
+            display: inline-block;
+          }
+
+          & > * + * {
+            margin-left: 15px;
+          }
+        `
+      )}
+    />
+          )}
+              </ClassNames>
+  )
+)
+
+// @ts-ignore
+export const Portal = ({ children }) => {
+  return typeof document === 'object'
+    ? ReactDOM.createPortal(children, document.body)
+    : null
+}
+
+export const Toolbar = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<OrNull<HTMLDivElement>>
+  ) => (
+      <ClassNames>
+          {({ css, cx }) => (
+              <Menu
+                  {...props}
+                  {...ref}
+                  className={cx(
+                      className,
+                      css`
+          position: relative;
+          padding: 1px 18px 17px;
+          margin: 0 -20px;
+          border-bottom: 2px solid #eee;
+          margin-bottom: 20px;
+        `
+                  )}
+              />
+          )}
+      </ClassNames>
+  )
+)
